ffd61e3877d584efeaebc06e5f4a3c37bd1aa000
Holger Benl <hbenl@evandor.de>
2025-01-13 10:31:44 +0100

    remove profile directory before running tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,3 +1,12 @@
 import { config } from 'dotenv';
+import fs from 'fs-extra';
 
 config({ path: 'src/test/.env'});
+
+export const mochaHooks = {
+	async beforeAll() {
+		if (process.env['FIREFOX_PROFILE_DIR'] && process.env['KEEP_PROFILE_CHANGES'] === 'true') {
+			await fs.remove(process.env['FIREFOX_PROFILE_DIR']);
+		}
+	}
+};
